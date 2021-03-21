4fc572e78d76286b04c0079b6759348027261260
Lavonne Jonnel <ljonnel-@vo-h3.msk.21-school.ru>
2021-03-21 17:33:50 +0300

    delete venv and end rating (without check)

--- ratings.py
+++ ratings.py
@@ -1,19 +1,48 @@
 from movielens_analysis import CsvParser
-from collections import Counter
+from collections import Counter, defaultdict
 from datetime import datetime
-average=None
 from movies import Movies as MoviesBase
+
+
+def mean(values):
+    if len(values) == 0:
+        return float('nan')
+    return sum(values) / len(values)
+
+
+def median(values):
+    if len(values) == 0:
+        return float('nan')
+    items = sorted(values)
+    if len(items) % 2 == 1:
+        return items[len(items)//2]
+    return (items[len(items)//2-1] + items[len(items)//2]) / 2
+
+
+def var(values):
+    if not len(values):
+        return float('nan')
+    mean_value = mean(values)
+    squared_diff = [(value - mean_value) ** 2 for value in values]
+    return sum(squared_diff) / len(squared_diff)
 
 
 class Ratings(CsvParser):
     """
     Analyzing data from ratings.csv
     """
-    def __init__(self, path_to_the_file):
+    def __init__(self, path_to_the_file, path_to_movies_file):
         """
         Put here any fields that you think you will need.
         """
         super().__init__(path_to_the_file)
+        self.filename_movies = path_to_movies_file
+
+    def get_users(self):
+        return Ratings.Users(self, self.filename_movies)
+
+    def get_movies(self):
+        return Ratings.Movies(self, self.filename_movies)
 
     def is_valid_file(self):
         return set(self.columns) == set('userId,movieId,rating,timestamp'.split(','))
@@ -25,7 +54,7 @@ class Ratings(CsvParser):
             yield film_info
 
     class Movies(MoviesBase):
-        def __init__(self, movies_filename, rating):
+        def __init__(self, rating, movies_filename):
             super().__init__(movies_filename)
             self.rating = rating
             self.movies_id2name = {}
@@ -47,7 +76,7 @@ class Ratings(CsvParser):
         def dist_by_rating(self):
             """
             The method returns a dict where the keys are ratings and the values are counts.
-         Sort it by ratings ascendingly.
+            Sort it by ratings ascendingly.
             """
             ratings_distribution = Counter()
             for film_info in self.rating.read_csv():
@@ -58,23 +87,46 @@ class Ratings(CsvParser):
             """
             The method returns top-n movies by the number of ratings. 
             It is a dict where the keys are movie titles and the values are numbers.
-     Sort it by numbers descendingly.
+            Sort it by numbers descendingly.
             """
             self.init_mapping_movies()
+            if n == -1:
+                n = len(self.movies_id2name)
             top_movies = Counter()
             for film_info in self.rating.read_csv():
                 top_movies[self.movies_id2name[film_info['movieId']]] += 1
             return dict(top_movies.most_common()[:n])
 
-        def top_by_ratings(self, n, metric=average):
+        def top_by_ratings(self, n, metric='average'):
             """
             The method returns top-n movies by the average or median of the ratings.
             It is a dict where the keys are movie titles and the values are metric values.
             Sort it by metric descendingly.
             The values should be rounded to 2 decimals.
             """
+            assert metric in ('average', 'median')
+            if n == -1:
+                n = len(self.movies_id2name)
+            top_movies = self._groupby_rating_by_film()
+            if metric == 'average':
+                top_movies = [(title, mean(ratings))
+                              for title, ratings in top_movies.items()]
+            else:
+                top_movies = [(title, median(ratings))
+                              for title, ratings in top_movies.items()]
+
+            return dict(
+                map(lambda x: (x[0], round(x[1], 2)),
+                    sorted(top_movies, key=lambda x: x[1], reverse=True)[:n]))
+
+        def _groupby_rating_by_film(self):
+            self.init_mapping_movies()
+            top_movies = defaultdict(list)
+            for film_info in self.rating.read_csv():
+                top_movies[self.movies_id2name[film_info['movieId']]].append(
+                    float(film_info['rating']))
             return top_movies
-        
+
         def top_controversial(self, n):
             """
             The method returns top-n movies by the variance of the ratings.
@@ -82,13 +134,41 @@ class Ratings(CsvParser):
           Sort it by variance descendingly.
             The values should be rounded to 2 decimals.
             """
-            return top_movies
+            if n == -1:
+                n = len(self.movies_id2name)
+            top_movies = self._groupby_rating_by_film()
+            top_movies = [(title, var(ratings))
+                          for title, ratings in top_movies.items()]
 
-    class Users:
+            return dict(
+                map(lambda x: (x[0], round(x[1], 2)),
+                    sorted(top_movies, key=lambda x: x[1], reverse=True)[:n]))
+
+    class Users(Movies):
         """
         In this class, three methods should work.
         The 1st returns the distribution of users by the number of ratings made by them.
         The 2nd returns the distribution of users by average or median ratings made by them.
         The 3rd returns top-n users with the biggest variance of their ratings.
-     Inherit from the class Movies. Several methods are similar to the methods from it.
+        Inherit from the class Movies. Several methods are similar to the methods from it.
         """
+
+        def _groupby_rating_by_film(self):
+            self.init_mapping_movies()
+            top_movies = defaultdict(list)
+            for film_info in self.rating.read_csv():
+                top_movies[film_info['userId']].append(
+                    float(film_info['rating']))
+            return top_movies
+
+        def top_controversial(self, n):
+            return super().top_controversial(n)
+
+        def dict_by_ratings(self, metric):
+            return super().top_by_ratings(-1, metric)
+
+        def dist_by_rating(self):
+            ratings_distribution = Counter()
+            for film_info in self.rating.read_csv():
+                ratings_distribution[film_info['userId']] += 1
+            return dict(ratings_distribution.most_common()[::-1])
